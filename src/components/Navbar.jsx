9f2db9b28190ed24be5bf8ddd6fb59e04a11ffc4
Amy <yambeddm@gmail.com>
2024-02-19 15:59:47 +0100

    additions to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,38 @@
 import React from 'react'
 import './style/Navbar.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+
 function Navbar() {
 
     return (
         <>
-        <header id="nav-wrapper">
-        <nav id="nav">
-          <div className="nav left">
-            <span className="gradient skew"><h1 className="logo un-skew"><a href="#home">Logo Here</a></h1></span>
-            <button id="menu" class="btn-nav"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-              <path fill="#fe206d" d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
-              </svg>
-            </button>
-          </div>
-          <div className="nav right">
-            <a href="#about" className="nav-link"><span className="nav-link-span"><span className="u-nav">About</span></span></a>
-            <a href="#work" className="nav-link"><span className="nav-link-span"><span className="u-nav">Work</span></span></a>
-            <a href="#contact" className="nav-link"><span className="nav-link-span"><span className="u-nav">Contact</span></span></a>
-          </div>
-        </nav>
-      </header>
-      <main>
-        <section id="home">
-    
-        </section>
-        <section id="about">
-    
-        </section>
-        <section id="work">
-    
-        </section>
-        <section id="contact">
-    
-        </section>
-      </main>
+      <header id="nav-wrapper">
+    <nav id="nav">
+      <div className="nav left">
+        <span className="gradient skew"><h1 className="logo un-skew"><a href="#home">Logo Here</a></h1></span>
+        <button id="menu" className="btn-nav"><span className="fa fa-bars"></span></button>
+      </div>
+      <div class="nav right">
+        {/* <a href="#home" class="nav-link active"><span class="nav-link-span"><span class="u-nav">Home</span></span></a> */}
+        <a href="#about" className="nav-link"><span className="nav-link-span"><span className="u-nav">femmes</span></span></a>
+        <a href="#work" className="nav-link"><span className="nav-link-span"><span className="u-nav">santé</span></span></a>
+        <a href="#contact" className="nav-link"><span className="nav-link-span"><span className="u-nav">écologie</span></span></a>
+      </div>
+    </nav>
+  </header>
+  <main>
+    <section id="home">
+
+    </section>
+    <section id="about">
+
+    </section>
+    <section id="work">
+
+    </section>
+    <section id="contact">
+
+    </section>
+  </main>
       </>
 
     );
